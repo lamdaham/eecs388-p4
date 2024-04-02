9e44b003bd8658c577f8929ac64b932aa06dbef5
Joseph <yijo@umich.edu>
2024-04-02 03:01:24 +0000

    Automatic commit by test script for ivanlam yijo

--- sol0.py
+++ sol0.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+
+sys.stdout.buffer.write(b'\x79\x69\x6A\x6F')
+sys.stdout.buffer.write(b'\x20'*(10-len(b'\x79\x69\x6A\x6F')))
+sys.stdout.buffer.write(b'\x41\x2B')
